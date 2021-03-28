87076ac7c4f4fd3719cf405b56c36468a8a2418d
Beatriz Penalva <biapenalva@gmail.com>
2021-03-28 02:46:28 -0300

    ajuste para tratativa de erros

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -1,68 +1,74 @@
-const database = require('../db/models')
+const database = require("../db/models");
 
 class UserController {
-
   static get(req, res, next) {
-    const users = database.Users.findAll()
+    const users = database.Users.findAll({
+      attributes: { exclude: ["password"] },
+    });
     users
       .then((result) => {
-        res.status(200).json(result)
+        res.status(200).json(result);
       })
-      .catch((err) => next({code:403, err}));
+      .catch((err) => next({ code: 403, err }));
   }
 
   static getById(req, res, next) {
-    const { usersId } = req.params
-    const user = database.Users.findAll({
-      where: {
-        id: Number(usersId)
+    const { usersId } = req.params;
+    const user = database.Users.findAll(
+      { attributes: { exclude: ["password"] } },
+      {
+        where: {
+          id: Number(usersId),
+        },
       }
-    });
+    );
     user
       .then((result) => {
-        res.status(200).json(result)
+        res.status(200).json(result);
       })
-      .catch((err) => next({code:403, err}));
+      .catch((err) => next({ code: 403, err }));
   }
 
   static post(req, res, next) {
-    const { name, email, password, role, restaurant } = req.body
+    const { name, email, password, role, restaurant } = req.body;
     const createUser = database.Users.create({
       name,
       email,
       password,
       role,
-      restaurant
+      restaurant,
     });
     createUser
       .then((result) => {
-        return res.status(201).json(result)
-    })
-      .catch(next)
+        return res.status(201).json(result);
+      })
+      .catch(next);
   }
 
   static update(req, res, next) {
-    const { usersId } = req.params
-    const { name, password, role } = req.body
-    const updateUser = database.Users.update({ name: name, password: password, role: role }, {
+    const { usersId } = req.params;
+    const { name, password, role } = req.body;
+    const updateUser = database.Users.update(
+      { name: name, password: password, role: role },
+      {
         where: {
-          id: Number(usersId)
-        }  
-    })
+          id: Number(usersId),
+        },
+      }
+    );
     updateUser
-      .then((result)=> {
-        return res.status(200).json(result)
+      .then((result) => {
+        return res.status(200).json(result);
       })
-      .catch(next)
-    }
-
+      .catch(next);
+  }
 
   static delete(req, res, next) {
-    const { usersId } = req.params 
+    const { usersId } = req.params;
     const user = database.Users.findAll({
       where: {
-        id: Number(usersId)
-      }
+        id: Number(usersId),
+      },
     });
     user
       .then((result) => {
@@ -72,4 +78,4 @@ class UserController {
   }
 }
 
-module.exports = UserController
+module.exports = UserController;

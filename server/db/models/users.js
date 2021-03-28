a8b5e79830de987e4ef6ffe95a57562079327745
Beatriz Penalva <biapenalva@gmail.com>
2021-03-28 02:46:04 -0300

    validações dos usuários

--- server/db/models/users.js
+++ server/db/models/users.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Users extends Model {
     /**
@@ -12,16 +10,86 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  Users.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    role: DataTypes.STRING,
-    restaurant: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Users',
-  });
+  }
+  Users.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        notEmpty: true,
+        validate: {
+          notEmpty: {
+            msg: "Username can not be empty",
+          },
+          notNull: {
+            msg: "Username is required",
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        isEmail: true,
+        notEmpty: true,
+        validate: {
+          notEmpty: {
+            msg: "E-mail can not be empty",
+          },
+          notNull: {
+            msg: "E-mail is required",
+          },
+          isEmail: {
+            msg: "E-mail is invalid",
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        notEmpty: true,
+        validate: {
+          min: 6,
+          max: 12,
+          notEmpty: {
+            msg: "Password can not be empty",
+          },
+          notNull: {
+            msg: "Password is required",
+          },
+        },
+      },
+      role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        notEmpty: true,
+        validate: {
+          notEmpty: {
+            msg: "Role can not be empty",
+          },
+          notNull: {
+            msg: "Role is required",
+          },
+        },
+      },
+      restaurant: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        notEmpty: true,
+        validate: {
+          notEmpty: {
+            msg: "Restaurant can not be empty",
+          },
+          notNull: {
+            msg: "Restaurant is required",
+          },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Users",
+    }
+  );
   return Users;
 };

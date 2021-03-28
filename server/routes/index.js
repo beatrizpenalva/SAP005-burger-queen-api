c7d66c3ef8149971d720a63c86e46f70a00df415
Beatriz Penalva <biapenalva@gmail.com>
2021-03-28 17:09:27 -0300

    criando modelos de produtos

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,11 @@
 const { Router } = require('express')
-// const ProductsRouter = require('./Products')
+const ProductsRouter = require('./Products')
 const UsersRouter = require('./Users')
 // const OrdersRouter = require('./Orders')
 
 const router = Router()
 
-// router.use('/products', ProductsRouter)
+router.use('/products', ProductsRouter)
 router.use('/users', UsersRouter)
 // router.use('/orders', OrdersRouter)
 
